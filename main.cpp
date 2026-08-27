23228ddba810213e12a6885d1fd1901bdc7d55ed
tajso <taj.sobot@gmail.com>
2026-08-27 07:38:55 +0200

    Winows library works, Go!Motion sensor can be comunicated with!

--- main.cpp
+++ main.cpp
@@ -1,90 +1,188 @@
-#include <cstdio>
+/*********************************************************************************
+
+Copyright (c) 2010, Vernier Software & Technology
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Vernier Software & Technology nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL VERNIER SOFTWARE & TECHNOLOGY BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+**********************************************************************************/
+// GoIO_DeviceCheck.cpp : Defines the entry point for the console application.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <memory.h>
+
+#ifdef TARGET_OS_WIN
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 #include <windows.h>
+#pragma warning(disable: 4996)
+#endif
+#ifdef TARGET_OS_LINUX
+#include <sys/time.h>
+#endif
+#ifdef TARGET_OS_MAC
+#include <Carbon/Carbon.h>
+#endif
+
+#define MAX_NUM_MEASUREMENTS 100
+
 #include "GoIO_DLL_interface.h"
 
-constexpr int    kMaxSamples    = 100;
-constexpr double kSamplePeriodS = 0.040;
+char *deviceDesc[8] = {"?", "?", "Go! Temp", "Go! Link", "Go! Motion", "?", "?", "Mini GC"};
 
-void InitGoIO()     { GoIO_Init(); }
-void ShutdownGoIO() { GoIO_Uninit(); }
+bool GetAvailableDeviceName(char *deviceName, gtype_int32 nameLength, gtype_int32 *pVendorId, gtype_int32 *pProductId);
+static void OSSleep(unsigned long msToSleep);
 
-bool FindGoMotion(char* deviceNameOut, gtype_int32 nameBufLen)
+int main(int argc, char* argv[])
 {
-    // GoIO's discovery is always a two-step "ask count, then fetch by index"
-    // pattern -- you'll see this same shape again if you ever add other
-    // sensor types. This is GetAvailableDeviceName() from the sample,
-    // narrowed to just Go!Motion's product id.
-    int numFound = GoIO_UpdateListOfAvailableDevices(VERNIER_DEFAULT_VENDOR_ID, CYCLOPS_DEFAULT_PRODUCT_ID);
-    if (numFound <= 0)
-        return false;
+	char deviceName[GOIO_MAX_SIZE_DEVICE_NAME];
+	gtype_int32 vendorId;		//USB vendor id
+	gtype_int32 productId;		//USB product id
+	char tmpstring[100];
+	gtype_uint16 MajorVersion;
+	gtype_uint16 MinorVersion;
+	char units[20];
+	char equationType = 0;
 
-    GoIO_GetNthAvailableDeviceName(deviceNameOut, nameBufLen,
-                                   VERNIER_DEFAULT_VENDOR_ID, CYCLOPS_DEFAULT_PRODUCT_ID, 0);
-    return true;
+	gtype_int32 rawMeasurements[MAX_NUM_MEASUREMENTS];
+	gtype_real64 volts[MAX_NUM_MEASUREMENTS];
+	gtype_real64 calbMeasurements[MAX_NUM_MEASUREMENTS];
+	gtype_int32 numMeasurements, i;
+	gtype_real64 averageCalbMeasurement;
+
+	printf("GoIO_DeviceCheck version 1.1\n");
+	
+	GoIO_Init();
+
+	GoIO_GetDLLVersion(&MajorVersion, &MinorVersion);
+	printf("This app is linked to GoIO lib version %d.%d .\n", MajorVersion, MinorVersion);
+
+	bool bFoundDevice = GetAvailableDeviceName(deviceName, GOIO_MAX_SIZE_DEVICE_NAME, &vendorId, &productId);
+	if (!bFoundDevice)
+		printf("No Go devices found.\n");
+	else
+	{
+		GOIO_SENSOR_HANDLE hDevice = GoIO_Sensor_Open(deviceName, vendorId, productId, 0);
+		if (hDevice != NULL)
+		{
+			printf("Successfully opened %s device %s .\n", deviceDesc[productId], deviceName);
+
+			unsigned char charId;
+			GoIO_Sensor_DDSMem_GetSensorNumber(hDevice, &charId, 0, 0);
+			printf("Sensor id = %d", charId);
+
+			GoIO_Sensor_DDSMem_GetLongName(hDevice, tmpstring, sizeof(tmpstring));
+			if (strlen(tmpstring) != 0)
+				printf("(%s)", tmpstring);
+			printf("\n");
+
+			GoIO_Sensor_SetMeasurementPeriod(hDevice, 0.040, SKIP_TIMEOUT_MS_DEFAULT);//40 milliseconds measurement period.
+			GoIO_Sensor_SendCmdAndGetResponse(hDevice, SKIP_CMD_ID_START_MEASUREMENTS, NULL, 0, NULL, NULL, SKIP_TIMEOUT_MS_DEFAULT);
+			OSSleep(1000); //Wait 1 second.
+
+			numMeasurements = GoIO_Sensor_ReadRawMeasurements(hDevice, rawMeasurements, MAX_NUM_MEASUREMENTS);
+			printf("%d measurements received after about 1 second.\n", numMeasurements);
+			averageCalbMeasurement = 0.0;
+			for (i = 0; i < numMeasurements; i++)
+			{
+				volts[i] = GoIO_Sensor_ConvertToVoltage(hDevice, rawMeasurements[i]);
+				calbMeasurements[i] = GoIO_Sensor_CalibrateData(hDevice, volts[i]);
+				averageCalbMeasurement += calbMeasurements[i];
+			}
+			if (numMeasurements > 1)
+				averageCalbMeasurement = averageCalbMeasurement/numMeasurements;
+
+			GoIO_Sensor_DDSMem_GetCalibrationEquation(hDevice, &equationType);
+			gtype_real32 a, b, c;
+			unsigned char activeCalPage = 0;
+			GoIO_Sensor_DDSMem_GetActiveCalPage(hDevice, &activeCalPage);
+			GoIO_Sensor_DDSMem_GetCalPage(hDevice, activeCalPage, &a, &b, &c, units, sizeof(units));
+			printf("Average measurement = %8.3f %s .\n", averageCalbMeasurement, units);
+
+			GoIO_Sensor_Close(hDevice);
+		}
+	}
+	GoIO_Uninit();
+	return 0;
 }
 
-GOIO_SENSOR_HANDLE OpenGoMotion(const char* deviceName)
+bool GetAvailableDeviceName(char *deviceName, gtype_int32 nameLength, gtype_int32 *pVendorId, gtype_int32 *pProductId)
 {
-    // TODO: look at the line right after GetAvailableDeviceName() succeeds in
-    // GoIO_DeviceCheck.cpp -- it's a single call to GoIO_Sensor_Open(). Same
-    // call here, swap in CYCLOPS_DEFAULT_PRODUCT_ID for the sample's productId.
-    return nullptr;
+	bool bFoundDevice = false;
+	deviceName[0] = 0;
+	int numSkips = GoIO_UpdateListOfAvailableDevices(VERNIER_DEFAULT_VENDOR_ID, SKIP_DEFAULT_PRODUCT_ID);
+	int numJonahs = GoIO_UpdateListOfAvailableDevices(VERNIER_DEFAULT_VENDOR_ID, USB_DIRECT_TEMP_DEFAULT_PRODUCT_ID);
+	int numCyclopses = GoIO_UpdateListOfAvailableDevices(VERNIER_DEFAULT_VENDOR_ID, CYCLOPS_DEFAULT_PRODUCT_ID);
+	int numMiniGCs = GoIO_UpdateListOfAvailableDevices(VERNIER_DEFAULT_VENDOR_ID, MINI_GC_DEFAULT_PRODUCT_ID);
+
+	if (numSkips > 0)
+	{
+		GoIO_GetNthAvailableDeviceName(deviceName, nameLength, VERNIER_DEFAULT_VENDOR_ID, SKIP_DEFAULT_PRODUCT_ID, 0);
+		*pVendorId = VERNIER_DEFAULT_VENDOR_ID;
+		*pProductId = SKIP_DEFAULT_PRODUCT_ID;
+		bFoundDevice = true;
+	}
+	else if (numJonahs > 0)
+	{
+		GoIO_GetNthAvailableDeviceName(deviceName, nameLength, VERNIER_DEFAULT_VENDOR_ID, USB_DIRECT_TEMP_DEFAULT_PRODUCT_ID, 0);
+		*pVendorId = VERNIER_DEFAULT_VENDOR_ID;
+		*pProductId = USB_DIRECT_TEMP_DEFAULT_PRODUCT_ID;
+		bFoundDevice = true;
+	}
+	else if (numCyclopses > 0)
+	{
+		GoIO_GetNthAvailableDeviceName(deviceName, nameLength, VERNIER_DEFAULT_VENDOR_ID, CYCLOPS_DEFAULT_PRODUCT_ID, 0);
+		*pVendorId = VERNIER_DEFAULT_VENDOR_ID;
+		*pProductId = CYCLOPS_DEFAULT_PRODUCT_ID;
+		bFoundDevice = true;
+	}
+	else if (numMiniGCs > 0)
+	{
+		GoIO_GetNthAvailableDeviceName(deviceName, nameLength, VERNIER_DEFAULT_VENDOR_ID, MINI_GC_DEFAULT_PRODUCT_ID, 0);
+		*pVendorId = VERNIER_DEFAULT_VENDOR_ID;
+		*pProductId = MINI_GC_DEFAULT_PRODUCT_ID;
+		bFoundDevice = true;
+	}
+
+	return bFoundDevice;
 }
 
-void CloseGoMotion(GOIO_SENSOR_HANDLE hDevice)
+void OSSleep(
+	unsigned long msToSleep)//milliseconds
 {
-    // TODO: GoIO_Sensor_Close(hDevice) -- one line, at the bottom of the sample's block.
-}
-
-void ConfigureAndStart(GOIO_SENSOR_HANDLE hDevice)
-{
-    // TODO: two calls back to back in the sample, right after Open succeeds:
-    //   GoIO_Sensor_SetMeasurementPeriod(hDevice, kSamplePeriodS, SKIP_TIMEOUT_MS_DEFAULT)
-    //   GoIO_Sensor_SendCmdAndGetResponse(hDevice, SKIP_CMD_ID_START_MEASUREMENTS,
-    //                                     NULL, 0, NULL, NULL, SKIP_TIMEOUT_MS_DEFAULT)
-}
-
-int ReadDistances(GOIO_SENSOR_HANDLE hDevice, double* metersOut, int maxSamples)
-{
-    // TODO: this is the sample's rawMeasurements -> volts -> calbMeasurements
-    // loop, just writing into metersOut instead of the sample's local arrays:
-    //   gtype_int32 raw[kMaxSamples];
-    //   int n = GoIO_Sensor_ReadRawMeasurements(hDevice, raw, maxSamples);
-    //   for i in [0, n):
-    //       double v = GoIO_Sensor_ConvertToVoltage(hDevice, raw[i]);
-    //       metersOut[i] = GoIO_Sensor_CalibrateData(hDevice, v);
-    //   return n;
-    return 0;
-}
-
-int main()
-{
-    InitGoIO();
-
-    char deviceName[GOIO_MAX_SIZE_DEVICE_NAME];
-    if (!FindGoMotion(deviceName, GOIO_MAX_SIZE_DEVICE_NAME))
-    {
-        printf("No Go!Motion found.\n");
-        ShutdownGoIO();
-        return 1;
-    }
-
-    GOIO_SENSOR_HANDLE hDevice = OpenGoMotion(deviceName);
-    if (hDevice == nullptr)
-    {
-        printf("Failed to open Go!Motion.\n");
-        ShutdownGoIO();
-        return 1;
-    }
-
-    ConfigureAndStart(hDevice);
-    Sleep(1000);
-
-    double distances[kMaxSamples];
-    int n = ReadDistances(hDevice, distances, kMaxSamples);
-    printf("Got %d distance readings.\n", n);
-
-    CloseGoMotion(hDevice);
-    ShutdownGoIO();
-    return 0;
+#ifdef TARGET_OS_WIN
+	::Sleep(msToSleep);
+#endif
+#ifdef TARGET_OS_LINUX
+  struct timeval tv;
+  unsigned long usToSleep = msToSleep*1000;
+  tv.tv_sec = usToSleep/1000000;
+  tv.tv_usec = usToSleep % 1000000;
+  select (0, NULL, NULL, NULL, &tv);
+#endif
+#ifdef TARGET_OS_MAC
+	AbsoluteTime absTime = ::AddDurationToAbsolute(msToSleep * durationMillisecond, ::UpTime());
+	::MPDelayUntil(&absTime);
+#endif
 }
