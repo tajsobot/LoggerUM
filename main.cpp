ea197af0249d4bdbab2e126b10ee6b08062d105b
tajso <taj.sobot@gmail.com>
2026-08-28 05:02:55 +0200

    Gui configured, labels, buttons, fields work!

--- main.cpp
+++ main.cpp
@@ -1,188 +1,106 @@
-/*********************************************************************************
-
-Copyright (c) 2010, Vernier Software & Technology
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of Vernier Software & Technology nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL VERNIER SOFTWARE & TECHNOLOGY BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-**********************************************************************************/
-// GoIO_DeviceCheck.cpp : Defines the entry point for the console application.
+// gui_main.cpp
 //
+// Bare ImGui + SDL3 + OpenGL3 window. Deliberately has zero sensor code --
+// the point of this file is to prove the GUI stack builds and runs on its
+// own, so if something breaks later you know which half (GUI vs. sensor)
+// is at fault.
 
-#include <stdio.h>
-#include <string.h>
-#include <memory.h>
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_opengl3.h"
 
-#ifdef TARGET_OS_WIN
-#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
-#include <windows.h>
-#pragma warning(disable: 4996)
-#endif
-#ifdef TARGET_OS_LINUX
-#include <sys/time.h>
-#endif
-#ifdef TARGET_OS_MAC
-#include <Carbon/Carbon.h>
-#endif
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_opengl.h>
+#include <cstdio>
 
-#define MAX_NUM_MEASUREMENTS 100
+int main() {
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+      printf("SDL_Init failed: %s\n", SDL_GetError());
+      return 1;
+  }
 
-#include "GoIO_DLL_interface.h"
+  // baseline that ImGui's OpenGL3 backend targets by default.
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
-char *deviceDesc[8] = {"?", "?", "Go! Temp", "Go! Link", "Go! Motion", "?", "?", "Mini GC"};
+  SDL_Window* window = SDL_CreateWindow(
+    "LoggerUM",
+    1280, 720,
+    SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 
-bool GetAvailableDeviceName(char *deviceName, gtype_int32 nameLength, gtype_int32 *pVendorId, gtype_int32 *pProductId);
-static void OSSleep(unsigned long msToSleep);
+  if (window == nullptr) {
+    printf("SDL_CreateWindow failed: %s\n", SDL_GetError());
+    SDL_Quit();
+    return 1;
+  }
 
-int main(int argc, char* argv[])
-{
-	char deviceName[GOIO_MAX_SIZE_DEVICE_NAME];
-	gtype_int32 vendorId;		//USB vendor id
-	gtype_int32 productId;		//USB product id
-	char tmpstring[100];
-	gtype_uint16 MajorVersion;
-	gtype_uint16 MinorVersion;
-	char units[20];
-	char equationType = 0;
+  SDL_GLContext glContext = SDL_GL_CreateContext(window);
+  if (glContext == nullptr) {
+    printf("SDL_GL_CreateContext failed: %s\n", SDL_GetError());
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 1;
+  }
 
-	gtype_int32 rawMeasurements[MAX_NUM_MEASUREMENTS];
-	gtype_real64 volts[MAX_NUM_MEASUREMENTS];
-	gtype_real64 calbMeasurements[MAX_NUM_MEASUREMENTS];
-	gtype_int32 numMeasurements, i;
-	gtype_real64 averageCalbMeasurement;
+  SDL_GL_MakeCurrent(window, glContext);
+  SDL_GL_SetSwapInterval(1); // vsync on
 
-	printf("GoIO_DeviceCheck version 1.1\n");
-	
-	GoIO_Init();
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGui::StyleColorsDark();
 
-	GoIO_GetDLLVersion(&MajorVersion, &MinorVersion);
-	printf("This app is linked to GoIO lib version %d.%d .\n", MajorVersion, MinorVersion);
+  ImGui_ImplSDL3_InitForOpenGL(window, glContext);
+  ImGui_ImplOpenGL3_Init("#version 150");
 
-	bool bFoundDevice = GetAvailableDeviceName(deviceName, GOIO_MAX_SIZE_DEVICE_NAME, &vendorId, &productId);
-	if (!bFoundDevice)
-		printf("No Go devices found.\n");
-	else
-	{
-		GOIO_SENSOR_HANDLE hDevice = GoIO_Sensor_Open(deviceName, vendorId, productId, 0);
-		if (hDevice != NULL)
-		{
-			printf("Successfully opened %s device %s .\n", deviceDesc[productId], deviceName);
+  bool running = true;
+  //main loop
+  while (running) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      ImGui_ImplSDL3_ProcessEvent(&event);
+      if (event.type == SDL_EVENT_QUIT) running = false;
+    }
 
-			unsigned char charId;
-			GoIO_Sensor_DDSMem_GetSensorNumber(hDevice, &charId, 0, 0);
-			printf("Sensor id = %d", charId);
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplSDL3_NewFrame();
+    ImGui::NewFrame();
 
-			GoIO_Sensor_DDSMem_GetLongName(hDevice, tmpstring, sizeof(tmpstring));
-			if (strlen(tmpstring) != 0)
-				printf("(%s)", tmpstring);
-			printf("\n");
+    // --- Bare-window content: just enough to confirm it's alive ---
+    static char textBuf[128] = "";
+    static int clickCount = 0;
 
-			GoIO_Sensor_SetMeasurementPeriod(hDevice, 0.040, SKIP_TIMEOUT_MS_DEFAULT);//40 milliseconds measurement period.
-			GoIO_Sensor_SendCmdAndGetResponse(hDevice, SKIP_CMD_ID_START_MEASUREMENTS, NULL, 0, NULL, NULL, SKIP_TIMEOUT_MS_DEFAULT);
-			OSSleep(1000); //Wait 1 second.
+    ImGui::Begin("LoggerUM");
+    if (ImGui::Button("Click me")) {
+      clickCount++;
+      printf("Button clicked! Count = %d\n", clickCount);
+    }
+    ImGui::SameLine();
+    ImGui::Text("Clicked %d times", clickCount);
 
-			numMeasurements = GoIO_Sensor_ReadRawMeasurements(hDevice, rawMeasurements, MAX_NUM_MEASUREMENTS);
-			printf("%d measurements received after about 1 second.\n", numMeasurements);
-			averageCalbMeasurement = 0.0;
-			for (i = 0; i < numMeasurements; i++)
-			{
-				volts[i] = GoIO_Sensor_ConvertToVoltage(hDevice, rawMeasurements[i]);
-				calbMeasurements[i] = GoIO_Sensor_CalibrateData(hDevice, volts[i]);
-				averageCalbMeasurement += calbMeasurements[i];
-			}
-			if (numMeasurements > 1)
-				averageCalbMeasurement = averageCalbMeasurement/numMeasurements;
+    ImGui::InputText("Label", textBuf, sizeof(textBuf));
+    if (ImGui::Button("Print text"))
+    {
+      printf("Text box contains: %s\n", textBuf);
+    }
 
-			GoIO_Sensor_DDSMem_GetCalibrationEquation(hDevice, &equationType);
-			gtype_real32 a, b, c;
-			unsigned char activeCalPage = 0;
-			GoIO_Sensor_DDSMem_GetActiveCalPage(hDevice, &activeCalPage);
-			GoIO_Sensor_DDSMem_GetCalPage(hDevice, activeCalPage, &a, &b, &c, units, sizeof(units));
-			printf("Average measurement = %8.3f %s .\n", averageCalbMeasurement, units);
+    ImGui::End();
+    // ----------------------------------------------------------------
+    ImGui::Render();
+    glViewport(0, 0, (int)ImGui::GetIO().DisplaySize.x, (int)ImGui::GetIO().DisplaySize.y);
+    glClearColor(0.1f, 0.1f, 0.12f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-			GoIO_Sensor_Close(hDevice);
-		}
-	}
-	GoIO_Uninit();
-	return 0;
-}
+    SDL_GL_SwapWindow(window);
+  }
 
-bool GetAvailableDeviceName(char *deviceName, gtype_int32 nameLength, gtype_int32 *pVendorId, gtype_int32 *pProductId)
-{
-	bool bFoundDevice = false;
-	deviceName[0] = 0;
-	int numSkips = GoIO_UpdateListOfAvailableDevices(VERNIER_DEFAULT_VENDOR_ID, SKIP_DEFAULT_PRODUCT_ID);
-	int numJonahs = GoIO_UpdateListOfAvailableDevices(VERNIER_DEFAULT_VENDOR_ID, USB_DIRECT_TEMP_DEFAULT_PRODUCT_ID);
-	int numCyclopses = GoIO_UpdateListOfAvailableDevices(VERNIER_DEFAULT_VENDOR_ID, CYCLOPS_DEFAULT_PRODUCT_ID);
-	int numMiniGCs = GoIO_UpdateListOfAvailableDevices(VERNIER_DEFAULT_VENDOR_ID, MINI_GC_DEFAULT_PRODUCT_ID);
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplSDL3_Shutdown();
+  ImGui::DestroyContext();
 
-	if (numSkips > 0)
-	{
-		GoIO_GetNthAvailableDeviceName(deviceName, nameLength, VERNIER_DEFAULT_VENDOR_ID, SKIP_DEFAULT_PRODUCT_ID, 0);
-		*pVendorId = VERNIER_DEFAULT_VENDOR_ID;
-		*pProductId = SKIP_DEFAULT_PRODUCT_ID;
-		bFoundDevice = true;
-	}
-	else if (numJonahs > 0)
-	{
-		GoIO_GetNthAvailableDeviceName(deviceName, nameLength, VERNIER_DEFAULT_VENDOR_ID, USB_DIRECT_TEMP_DEFAULT_PRODUCT_ID, 0);
-		*pVendorId = VERNIER_DEFAULT_VENDOR_ID;
-		*pProductId = USB_DIRECT_TEMP_DEFAULT_PRODUCT_ID;
-		bFoundDevice = true;
-	}
-	else if (numCyclopses > 0)
-	{
-		GoIO_GetNthAvailableDeviceName(deviceName, nameLength, VERNIER_DEFAULT_VENDOR_ID, CYCLOPS_DEFAULT_PRODUCT_ID, 0);
-		*pVendorId = VERNIER_DEFAULT_VENDOR_ID;
-		*pProductId = CYCLOPS_DEFAULT_PRODUCT_ID;
-		bFoundDevice = true;
-	}
-	else if (numMiniGCs > 0)
-	{
-		GoIO_GetNthAvailableDeviceName(deviceName, nameLength, VERNIER_DEFAULT_VENDOR_ID, MINI_GC_DEFAULT_PRODUCT_ID, 0);
-		*pVendorId = VERNIER_DEFAULT_VENDOR_ID;
-		*pProductId = MINI_GC_DEFAULT_PRODUCT_ID;
-		bFoundDevice = true;
-	}
-
-	return bFoundDevice;
-}
-
-void OSSleep(
-	unsigned long msToSleep)//milliseconds
-{
-#ifdef TARGET_OS_WIN
-	::Sleep(msToSleep);
-#endif
-#ifdef TARGET_OS_LINUX
-  struct timeval tv;
-  unsigned long usToSleep = msToSleep*1000;
-  tv.tv_sec = usToSleep/1000000;
-  tv.tv_usec = usToSleep % 1000000;
-  select (0, NULL, NULL, NULL, &tv);
-#endif
-#ifdef TARGET_OS_MAC
-	AbsoluteTime absTime = ::AddDurationToAbsolute(msToSleep * durationMillisecond, ::UpTime());
-	::MPDelayUntil(&absTime);
-#endif
+  SDL_GL_DestroyContext(glContext);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
 }
